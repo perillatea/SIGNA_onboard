5ec384ca0256a3f0829767b42a3c1f68feda8267
perillatea <53457503+perillatea@users.noreply.github.com>
2020-06-08 14:51:56 +0900

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: //home/gnss/novatel_dw
+# Build directory: //home/gnss/novatel_dw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTestsIntest_novatel "novatel_tests")
